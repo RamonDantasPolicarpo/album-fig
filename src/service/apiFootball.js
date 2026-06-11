36e7b110738fe8c50867481bdc2bda5f4aee12df
Ramon Dantas Policarpo <ramonpolicarpo36@gmail.com>
2026-06-10 21:05:23 -0300

    Adiciona getPaises para buscar países
    Introduz uma função async getPaises que chama o endpoint /teams/countries na API de futebol API-SPORTS. A função usa a API key do import.meta.env para configurar os headers da requisição e retorna o payload parseado da resposta (dados.response).

--- src/service/apiFootball.js
+++ src/service/apiFootball.js
@@ -1,2 +1,15 @@
 const BASE_URL = 'https://v3.football.api-sports.io';
 const API_KEY = import.meta.env.development.VITE_API_TOKEN;
+
+export async function getPaises() {
+    const response = await fetch(`${BASE_URL}/teams/countries`, {
+        method: get,
+        headers: {
+            'x-apisports-key': API_KEY,
+            'x-rapidapi-host': 'v3.football.api-sports.io'
+        }
+    });
+
+    const dados = await response.json();
+    return dados.response;
+}
